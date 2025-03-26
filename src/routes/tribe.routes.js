38dfe377194359edc373b1c712a0153402de4a16
ianton202 <ignacioanton613@gmail.com>
2025-03-26 15:44:37 -0300

    incorporacion de express-validator

--- src/routes/tribe.routes.js
+++ src/routes/tribe.routes.js
@@ -1,5 +1,7 @@
-import {Router} from 'express'
+import { Router } from 'express'
+import { body, param } from 'express-validator'
 import { authRequired } from '../middlewares/validateToken.js'
+import { handleInputErrors } from '../middlewares/validateInputErrors.js'
 import {
     getTribes,
     getTribeById,
@@ -11,12 +13,34 @@ import {
 const router = Router()
 
 router.get('/', authRequired, getTribes)
-router.get('/:id', authRequired, getTribeById)
+router.get('/:id', authRequired,
+    param('id')
+        .isMongoId().withMessage('Invalid ID'),
+    handleInputErrors,
+    getTribeById)
 
-router.post('/', authRequired, createTribe)
+router.post('/', authRequired, 
+    body('name')
+        .notEmpty().isString().withMessage('Name cant be empty and must be a string'),
+    body('collaborator_id')
+        .optional().isMongoId().withMessage('Invalid collaborator ID'),
+    handleInputErrors,
+    createTribe)
 
-router.delete('/:id', authRequired, deleteTribe)
+router.delete('/:id', authRequired, 
+    param('id')
+        .isMongoId().withMessage('Invalid ID'),
+    handleInputErrors,
+    deleteTribe)
 
-router.put('/:id', authRequired, updateTribe)
+router.put('/:id', authRequired, 
+    param('id')
+        .isMongoId().withMessage('Invalid ID'),
+    body('name')
+        .optional().isString().withMessage('Name must be a string'),
+    body('collaborator_id')
+        .optional().isMongoId().withMessage('Invalid collaborator ID'),
+    handleInputErrors,
+    updateTribe)
 
 export default router
