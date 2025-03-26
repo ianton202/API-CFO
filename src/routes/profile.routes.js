df644d0651f23aa549276e389e0a44b810803cd8
ianton202 <ignacioanton613@gmail.com>
2025-03-26 15:38:28 -0300

    incorporacion de express-validator

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
+import { body, param } from 'express-validator'
 import { authRequired } from '../middlewares/validateToken.js'
+import { handleInputErrors } from '../middlewares/validateInputErrors.js'
 import {
     getProfiles,
     getProfileById,
@@ -11,12 +13,29 @@ import {
 const router = Router()
 
 router.get('/', authRequired, getProfiles)
-router.get('/:id', authRequired, getProfileById)
+router.get('/:id', authRequired, 
+    param('id')
+        .isMongoId().withMessage('Invalid ID'),
+    handleInputErrors,
+    getProfileById)
 
-router.post('/', authRequired, createProfile)
+router.post('/', authRequired,
+    body('name')
+        .notEmpty().isString().withMessage('Name cant be empty and must be a string'),
+    createProfile)
 
-router.delete('/:id', authRequired, deleteProfile)
+router.delete('/:id', authRequired,
+    param('id')
+        .isMongoId().withMessage('Invalid ID'),
+    handleInputErrors,
+    deleteProfile)
 
-router.put('/:id', authRequired, updateProfile)
+router.put('/:id', authRequired,
+    param('id')
+        .isMongoId().withMessage('Invalid ID'),
+    body('name')
+        .optional().isString().withMessage('Name must be a string'),
+    handleInputErrors,
+    updateProfile)
 
 export default router
--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
+import { body, param } from 'express-validator'
 import { authRequired } from '../middlewares/validateToken.js'
+import { handleInputErrors } from '../middlewares/validateInputErrors.js'
 import {
     getProjects,
     getProjectById,
@@ -11,12 +13,42 @@ import {
 const router = Router()
 
 router.get('/', authRequired, getProjects)
-router.get('/:id', authRequired, getProjectById)
+router.get('/:id', authRequired,
+    param('id')
+        .isMongoId().withMessage('Invalid ID'),
+    handleInputErrors,
+    getProjectById)
 
-router.post('/', authRequired, createProject)
+router.post('/', authRequired,
+    body('name')
+        .notEmpty().isString().withMessage('Name cant be empty and must be a string'),
+    body('client_id')
+        .optional().isMongoId().withMessage('Invalid client ID'),
+    body('tribe_id')
+        .optional().isMongoId().withMessage('Invalid tribe ID'),
+    body('collaborator_id')
+        .optional().isMongoId().withMessage('Invalid collaborator ID'),
+    handleInputErrors,    
+    createProject)
 
-router.delete('/:id', authRequired, deleteProject)
+router.delete('/:id', authRequired,
+    param('id')
+        .isMongoId().withMessage('Invalid ID'),
+    handleInputErrors,
+    deleteProject)
 
-router.put('/:id', authRequired, updateProject)
+router.put('/:id', authRequired,
+    param('id')
+        .isMongoId().withMessage('Invalid ID'),
+        body('name')
+        .optional().isString().withMessage('Name must be a string'),
+    body('client_id')
+        .optional().isMongoId().withMessage('Invalid client ID'),
+    body('tribe_id')
+        .optional().isMongoId().withMessage('Invalid tribe ID'),
+    body('collaborator_id')
+        .optional().isMongoId().withMessage('Invalid collaborator ID'),
+    handleInputErrors,
+    updateProject)
 
 export default router;
